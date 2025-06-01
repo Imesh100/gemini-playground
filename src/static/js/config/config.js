c9776a26dccb63d53fa488585737b19efc711e56
Imesh100 <93514170+Imesh100@users.noreply.github.com>
2025-06-01 20:24:35 +0530

    Update config.js

--- src/static/js/config/config.js
+++ src/static/js/config/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
     API: {
-        VERSION: 'v1alpha',
+        VERSION: 'v1beta',
         MODEL_NAME: 'models/gemini-2.5-pro-preview-tts' // Gemini 2.5 Flash preview model
     },
     // You can change the system instruction to your liking
